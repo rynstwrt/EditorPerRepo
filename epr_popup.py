34b85495e460022554715df6420a72f90b6b59f0
Ryn. <rynstwrt17@gmail.com>
2025-03-03 08:06:52 -0600

    Add popup design and implementation

--- epr_popup.py
+++ epr_popup.py
@@ -3,37 +3,49 @@ import webview
 
 
 class EprPopupTypes(Enum):
-    INVALID_DIR = auto()
-    INVALID_EDITOR = auto()
+    TARGET_DIR_NOT_SPECIFIED = auto()
+    TARGET_DIR_NOT_EXIST = auto()
+    EDITOR_NOT_EXIST = auto()
 
 
 class EprPopup:
     __POPUP_TITLE = "EPR Popup"
     __POPUP_HTML_PATH = "./static/popup.html"
-    __POPUP_SIZE = (350, 250)
-    __POPUP_MESSAGES = {
-        EprPopupTypes.INVALID_DIR: "dir",
-        EprPopupTypes.INVALID_EDITOR: "editor"
+    __POPUP_SIZE = (550, 300)
+    __POPUP_TEXTS = {
+        EprPopupTypes.TARGET_DIR_NOT_SPECIFIED: "The target directory to open was not specified!",
+        EprPopupTypes.TARGET_DIR_NOT_EXIST: "The target directory does not exist!",
+        EprPopupTypes.EDITOR_NOT_EXIST: "The chosen editor does not exist!"
     }
 
 
     def __init__(self):
         self.popup_type = None
+        self.given_path = None
         self.window: webview.Window = webview.create_window(self.__POPUP_TITLE, self.__POPUP_HTML_PATH,
                                                             width=self.__POPUP_SIZE[0], height=self.__POPUP_SIZE[1],
                                                             resizable=False, frameless=True, easy_drag=True)
 
 
     def __create_content(self):
-        self.window.dom.get_element("#popup-text").text = self.__POPUP_MESSAGES[self.popup_type]
+        self.window.dom.get_element("#popup-text").text = self.__POPUP_TEXTS[self.popup_type]
+
+        popup_details = self.window.dom.get_element("#popup-details")
+        if not self.given_path:
+            popup_details.style["display"] = "none"
+            return
+
+        popup_details.text = f'({self.given_path.replace("\\", "\\\\")})'
 
 
     def __bind_events(self, _):
         self.window.dom.get_element("#exit-button").on("click", lambda e: self.window.destroy())
 
 
-    def show(self, popup_type: EprPopupTypes):
+    def show(self, popup_type: EprPopupTypes, given_path: str = None):
         self.popup_type = popup_type
+        self.given_path = given_path
+
         webview.DRAG_REGION_SELECTOR = "header"
         self.window.events.loaded += self.__create_content
         webview.start(self.__bind_events, self.window, ssl=True)
--- main.pyw
+++ main.pyw
@@ -10,11 +10,13 @@ import subprocess
 def run():
     args = sys.argv[1:]
     if not args:
-        return print("No target directory was specified!")
+        print("No target directory was specified!")
+        return EprPopup().show(EprPopupTypes.TARGET_DIR_NOT_SPECIFIED)
 
     target_dir_path = Path(expandvars(args[0])).resolve()
     if not target_dir_path.exists():
-        return print("Target repo dir does not exist!")
+        print("Target repo dir does not exist!")
+        return EprPopup().show(EprPopupTypes.TARGET_DIR_NOT_EXIST, given_path=str(target_dir_path))
 
     print("Target dir:", target_dir_path)
 
@@ -32,12 +34,12 @@ def run():
     editor_path = Path(target_dir_editor)
     if not editor_path.exists():
         # TODO: Keep reopening until a valid selection is made OR until canceled
-        return print(f"Selected editor for this repo ({editor_path}) does not exist!")
+        print(f"Selected editor for this repo ({editor_path}) does not exist!")
+        return EprPopup().show(EprPopupTypes.EDITOR_NOT_EXIST, given_path=str(target_dir_path))
 
     print(f"Found editor for {target_dir_path}! {target_dir_editor}")
     subprocess.Popen([editor_path, target_dir_path])
 
 
 if __name__ == "__main__":
-    EprPopup().show(EprPopupTypes.INVALID_DIR)
-    # run()
+    run()
