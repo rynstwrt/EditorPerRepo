884fcd606ec1b0f1a775c9823adec97bdb40e388
Ryn. <rynstwrt17@gmail.com>
2025-03-05 17:47:38 -0600

    Add erp_gui.py

--- src/erp_gui.py
+++ src/erp_gui.py
@@ -1,3 +1,49 @@
+from constants import *
+import FreeSimpleGUI as sg
+
+
 class ErpGui():
-    def __init__(self):
-        print()
+    def __init__(self, list_menu_items):
+        self.list_menu_items = list_menu_items
+
+
+    def create_window(self):
+        layout = [
+            [sg.Text("Please select an editor to open this directory:")],
+            [
+                sg.Listbox(key=EDITOR_LIST_KEY,
+                           values=self.list_menu_items,
+                           expand_x=True,
+                           expand_y=True,
+                           font=(FONT[0], FONT[1] - 1),
+                           select_mode=sg.SELECT_MODE_SINGLE,
+                           pad=((5, 5), (7, 7)))
+            ],
+            [
+                sg.Button("Cancel",
+                          size=SMALL_BUTTON_SIZE),
+                sg.Button(key=OPEN_CONFIG_KEY,
+                          button_text="Config",
+                          size=SMALL_BUTTON_SIZE),
+                sg.Button(key=SUBMIT_KEY,
+                          button_text="Submit",
+                          expand_x=True)
+            ],
+        ]
+
+        window = sg.Window(WINDOW_TITLE,
+                           layout=layout,
+                           size=DEFAULT_WINDOW_SIZE,
+                           element_justification="c",
+                           font=FONT,
+                           margins=(7, 7))
+
+        return window
+
+
+    @staticmethod
+    def make_warning_popup(reason):
+        sg.popup(title=WARNING_POPUP_TITLE,
+                 any_key_closes=True,
+                 custom_text=reason.upper(),
+                 button_type=sg.POPUP_BUTTONS_ERROR)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from constants import *
 from config_manager import ConfigManager
-
-
-def make_warning_popup(reason):
-    sg.popup(title=WARNING_POPUP_TITLE,
-             any_key_closes=True,
-             custom_text=reason.upper(),
-             button_type=sg.POPUP_BUTTONS_ERROR)
+from src.erp_gui import ErpGui
 
 
 def on_submit_button_press(selected_editor):
@@ -17,49 +11,15 @@ def on_open_config_press(selected_editor):
     print("open config in", selected_editor)
 
 
-def create_window(list_menu_items):
-    layout = [
-        [sg.Text("Please select an editor to open this directory:")],
-        [
-            sg.Listbox(key=EDITOR_LIST_KEY,
-                       values=list_menu_items,
-                       expand_x=True,
-                       expand_y=True,
-                       font=(FONT[0], FONT[1] - 1),
-                       select_mode=sg.SELECT_MODE_SINGLE,
-                       pad=((5, 5), (7, 7)))
-        ],
-        [
-            sg.Button("Cancel",
-                      size=SMALL_BUTTON_SIZE),
-            sg.Button(key=OPEN_CONFIG_KEY,
-                      button_text="Config",
-                      size=SMALL_BUTTON_SIZE),
-            sg.Button(key=SUBMIT_KEY,
-                      button_text="Submit",
-                      expand_x=True)
-        ],
-    ]
-
-    window = sg.Window(WINDOW_TITLE,
-                       layout=layout,
-                       size=DEFAULT_WINDOW_SIZE,
-                       element_justification="c",
-                       font=FONT,
-                       margins=(7, 7))
-
-    return window
-
-
 def main():
     config_manager = ConfigManager(CONFIG_FILE)
     success, config_data = config_manager.load_config()
 
     if not success:
-        return make_warning_popup(str(config_data))
+        return ErpGui.make_warning_popup(str(config_data))
 
     editor_paths = [editor["path"] for editor in config_data]
-    window = create_window(editor_paths)
+    window = ErpGui(editor_paths).create_window()
 
     while True:
         event, values = window.read()
@@ -70,7 +30,7 @@ def main():
         if event in [SUBMIT_KEY, OPEN_CONFIG_KEY]:
             selected_editor = window[EDITOR_LIST_KEY].get()
             if not selected_editor:
-                make_warning_popup("No editor is selected!")
+                ErpGui.make_warning_popup("No editor is selected!")
                 continue
 
             if event == SUBMIT_KEY:
