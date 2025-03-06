a32435fe3c4b5fdecb1ac6fd5cccf466fa739432
Ryn. <rynstwrt17@gmail.com>
2025-03-05 18:06:02 -0600

    Add config and submit button functionality

--- src/config_manager.py
+++ src/config_manager.py
@@ -5,15 +5,15 @@ from pathlib import Path
 class ConfigManager:
     def __init__(self, config_file):
         self.config_file = config_file
+        self.config_path = Path(__file__).parent / Path(self.config_file)
 
 
     def load_config(self):
-        config_path = Path(__file__).parent / Path(self.config_file)
-        if not config_path.exists():
-            return False, f"Error finding config! Path given: {config_path}"
+        if not self.config_path.exists():
+            return False, f"Error finding config! Path given: {self.config_path}"
 
         try:
-            with open(config_path, "rb") as file:
+            with open(self.config_path, "rb") as file:
                 config_data = toml.load(file)
                 file.close()
         except Exception as err:
@@ -25,3 +25,7 @@ class ConfigManager:
             return False, "No editors were found in the config!"
 
         return True, editors
+
+
+    def get_config_path(self):
+        return self.config_path
--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
+import sys
+import subprocess
+from os.path import expandvars
+from pathlib import Path
 from constants import *
 from config_manager import ConfigManager
-from src.erp_gui import ErpGui
+from erp_gui import ErpGui
 
 
-def on_submit_button_press(selected_editor):
-    print("ayy", selected_editor)
+def on_submit_button_press(selected_editor, target_path):
+    subprocess.Popen([selected_editor, target_path])
 
 
-def on_open_config_press(selected_editor):
-    print("open config in", selected_editor)
+def on_open_config_press(selected_editor, config_path):
+    print(f"open {config_path} in {selected_editor}")
+    subprocess.Popen([selected_editor, config_path])
 
 
 def main():
+    args = sys.argv[1:]
+    if not args:
+        return ErpGui.make_warning_popup("No path given!")
+
+    given_path = Path.cwd().joinpath(Path(args[0])).resolve()
+    if not given_path.is_dir():
+        return ErpGui.make_warning_popup("Given path is not a directory!")
+
+    target_path = expandvars(given_path)
+
     config_manager = ConfigManager(CONFIG_FILE)
     success, config_data = config_manager.load_config()
 
@@ -28,15 +43,18 @@ def main():
             break
 
         if event in [SUBMIT_KEY, OPEN_CONFIG_KEY]:
-            selected_editor = window[EDITOR_LIST_KEY].get()
-            if not selected_editor:
+            selected_editor_given_path = window[EDITOR_LIST_KEY].get()
+            if not selected_editor_given_path:
                 ErpGui.make_warning_popup("No editor is selected!")
                 continue
 
+            selected_editor = Path(expandvars(selected_editor_given_path[0]))
             if event == SUBMIT_KEY:
-                on_submit_button_press(selected_editor[0])
+                on_submit_button_press(selected_editor, target_path)
             elif event == OPEN_CONFIG_KEY:
-                on_open_config_press(selected_editor[0])
+                on_open_config_press(selected_editor, config_manager.get_config_path())
+
+            break
 
     window.close()
 
